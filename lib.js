fb39b3e4f7abdf032fc79e5ee0caafbdab869c80
rktella <ravi.tella@yahoo.com>
2019-12-15 07:36:05 -0500

    Update lib.js

--- lib.js
+++ lib.js
@@ -2,14 +2,4 @@ var commonUtils1=() => {
     var myFunction1 = () => {
        console.log("edf");
     }
-    var myFunction2 = () => {
-        
-        console.log("zyx");
-    }
-    return {
-        myPackage: {
-            myFunction1,
-            myFunction2
-        }
-    };
 }
